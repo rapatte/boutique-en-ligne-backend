35144142d32ba363f55c46c220148432d2fae79e
AntoineG <antoine.gayoso@gmail.com>
2024-11-27 15:36:06 +0100

    build: architecture revue

--- src/domain/ports/product-repository.port.ts
+++
@@ -1,8 +0,0 @@
-import { Product } from "../entities/product.entity";
-
-export interface ProductRepositoryPort {
-    create(product: Product): Promise<Product>;
-    findAll(): Promise<Product[]>;
-    findById(id: string): Promise<Product | null>;
-  }
-  
