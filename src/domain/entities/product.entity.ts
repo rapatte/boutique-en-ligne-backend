83b3acdcedbfcb7097eefef7cb25977766bc88fa
AntoineG <antoine.gayoso@gmail.com>
2024-11-27 15:42:57 +0100

    feat: domain entities

---
+++ src/domain/entities/product.entity.ts
@@ -0,0 +1,8 @@
+export class ProductEntity {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
---
+++ src/domain/entities/user.entity.ts
@@ -0,0 +1,8 @@
+export class UserEntity {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
