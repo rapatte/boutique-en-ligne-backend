2e4f494279d56b99ef724f1e731269aa923ffe11
AntoineG <antoine.gayoso@gmail.com>
2024-11-28 18:33:16 +0100

    fix: using configService

--- src/infrastructure/database/typeorm/typeorm-config.ts
+++ src/infrastructure/database/typeorm/typeorm-config.ts
@@ -5,10 +5,11 @@ import { ProductEntity } from "./entities/product.entity";
 
 export const typeOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
     type: 'mysql',
-    host: process.env.DATABASE_HOST,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    host: configService.get<string>(process.env.DATABASE_HOST),
+    port: configService.get<number>(process.env.DATABASE_PORT),
+    username: configService.get<string>(process.env.DATABASE_USERNAME),
+    password: configService.get<string>(process.env.DATABASE_PASSWORD),
+    database: configService.get<string>(process.env.DATABASE_NAME),
     entities: [UserEntity, ProductEntity],
     synchronize: true, // A DESACTIVER EN PROD
 })
