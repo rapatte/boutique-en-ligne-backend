315b2f500047ca3fde3ee4e2e3c8dcb413bf2811
AntoineG <antoine.gayoso@gmail.com>
2024-11-29 19:11:39 +0100

    feat: cas utilisation suppression utilisateur avec id

---
+++ src/application/user/{use-cases}/delete-user.use-case.ts
@@ -0,0 +1,23 @@
+import { Injectable, InternalServerErrorException, NotFoundException } from "@nestjs/common";
+import { UserRepositoryPort } from "src/domain/ports/user-repository.port";
+
+@Injectable()
+export class DeleteUserUseCase {
+    constructor(private readonly userRepository: UserRepositoryPort) { }
+
+    async execute(id: number): Promise<boolean> {
+        try {
+            const user = await this.userRepository.findOneById(id);
+            if (!user) {
+                throw new NotFoundException(`Suppression impossible car l'utilisateur avec l'id ${id} est introuvable.`)
+            }
+
+            return await this.userRepository.delete(id);
+        } catch (error) {
+            if (error instanceof NotFoundException) {
+                throw error;
+            }
+            throw new InternalServerErrorException("Erreur serveur lors de la suppression de l'utilisateur");
+        }
+    }
+}
