4e1bf744fc04a3cfd09368fb7e92e219873e4763
AntoineG <antoine.gayoso@gmail.com>
2024-11-29 16:27:56 +0100

    feat: cas utilisation recuperer un produit avec son id

---
+++ src/application/product/{use-cases}/getonebyid-product.use-case.ts
@@ -0,0 +1,24 @@
+import { Injectable, InternalServerErrorException, NotFoundException } from "@nestjs/common";
+import { ProductEntity } from "src/domain/entities/product.entity";
+import { ProductRepositoryPort } from "src/domain/ports/product-repository.port";
+
+@Injectable()
+export class GetOneProductById {
+    constructor(private readonly productRepository: ProductRepositoryPort) {}
+
+    async execute(id: number): Promise<ProductEntity> {
+        try {
+            const product = await this.productRepository.findOneById(id);
+            if (!product) {
+                throw new NotFoundException(`Le produit avec l'id ${id} est introuvable.`)
+            }
+
+            return this.productRepository.findOneById(id);
+        } catch (error) {
+            if (error instanceof NotFoundException) {
+                throw error;
+            }
+            throw new InternalServerErrorException('Erreur serveur lors de la récuparation du produit.')
+        }
+    }
+}
