cecc9b86d52d4596122923f6ffc11efb7fd4138f
AntoineG <antoine.gayoso@gmail.com>
2024-11-29 18:11:08 +0100

    feat: cas utilisation recuperation utilisateur avec id

---
+++ src/application/user/{use-cases}/getonebyid-user.use-case.ts
@@ -0,0 +1,24 @@
+import { Injectable, InternalServerErrorException, NotFoundException } from "@nestjs/common";
+import { UserEntity } from "src/domain/entities/user.entity";
+import { UserRepositoryPort } from "src/domain/ports/user-repository.port";
+
+@Injectable()
+export class GetOneUserById {
+    constructor(private readonly userRepository: UserRepositoryPort) { }
+
+    async execute(id: number): Promise<UserEntity> {
+        try {
+            const user = await this.userRepository.findOneById(id);
+            if (!user) {
+                throw new NotFoundException(`Aucun utilisateur avec l'id ${id} n'a été trouvé.`)
+            }
+
+            return this.userRepository.findOneById(id);
+        } catch (error) {
+            if (error instanceof NotFoundException) {
+                throw error;
+            }
+            throw new InternalServerErrorException("Erreur serveur lors de la récupération de l'utilisateur");
+        }
+    }
+}
