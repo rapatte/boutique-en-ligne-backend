0f5b15355d2b524a0733046301cc0fa3ce69bbd1
AntoineG <antoine.gayoso@gmail.com>
2024-12-10 16:47:06 +0100

    feat: user mapper and user dto

---
+++ src/shared/dtos/user.dto.ts
@@ -0,0 +1,10 @@
+export class UserDto {
+    lastname: string;
+    firstname: string;
+    country: string;
+    street: string;
+    postcode: string;
+    city: string;
+    email: string;
+    password: string;
+}
--- src/shared/utils/mappers.ts
+++ src/shared/utils/mappers.ts
@@ -1,5 +1,7 @@
 import { ProductEntityDomain } from 'src/domain/entities/product.entity';
+import { UserEntityDomain } from 'src/domain/entities/user.entity';
 import { ProductEntity } from 'src/infrastructure/database/typeorm/entities/product.entity';
+import { UserEntity } from 'src/infrastructure/database/typeorm/entities/user.entity';
 
 export class ProductMapper {
     static toDomain(product: ProductEntity): ProductEntityDomain {
@@ -19,5 +21,35 @@ export class ProductMapper {
         productEntity.price = product.price;
         productEntity.description = product.description;
         return productEntity;
+    }
+}
+
+export class UserMapper {
+    static toDomain(user: UserEntity): UserEntityDomain {
+        const userDomain = new UserEntityDomain({
+            id: user.id,
+            lastname: user.lastname,
+            firstname: user.firstname,
+            country: user.country,
+            street: user.street,
+            postcode: user.postcode,
+            city: user.city,
+            email: user.email,
+            password: user.password
+        })
+        return userDomain;
+    }
+
+    static toPersistencce(user: UserEntityDomain): UserEntity {
+        const userEntity = new UserEntity();
+        userEntity.id = user.id;
+        userEntity.lastname = user.lastname;
+        userEntity.firstname = user.firstname;
+        userEntity.country = user.country;
+        userEntity.street = user.street;
+        userEntity.postcode = user.postcode;
+        userEntity.email = user.email;
+        userEntity.password = user.password;
+        return userEntity;
     }
 }
